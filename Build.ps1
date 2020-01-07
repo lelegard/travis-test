57f88f1d2141735c38b45d2ec02ff745ebd8ad9d
Thierry Lelegard <thierry@lelegard.fr>
2020-01-07 15:14:02 +0100

    Initial

---
+++ Build.ps1
@@ -0,0 +1,9 @@
+﻿Set-StrictMode -Version 3
+if (((Get-ExecutionPolicy) -ne "Unrestricted") -and ((Get-ExecutionPolicy) -ne "RemoteSigned")) {
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force -ErrorAction:SilentlyContinue
+}
+Import-Module -Force -Name (Join-Path $PSScriptRoot Build-Common.psm1)
+
+$MSBuild = (Find-MSBuild)
+
+$MSBuild
---
+++ README.txt
@@ -0,0 +1,1 @@
+Test playground for Travis CI configuration files.
