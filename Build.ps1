b82374913569f5ebb2a52ddf26e4f764bf42e5ae
Thierry Lelegard <thierry@lelegard.fr>
2020-01-07 16:08:00 +0100

    4

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,16 @@
-﻿Set-StrictMode -Version 3
+﻿param(
+    [switch]$GitPull = $false,
+    [switch]$Installer = $false,
+    [switch]$NoLowPriority = $false,
+    [switch]$Debug = $false,
+    [switch]$Release = $false,
+    [switch]$Win32 = $false,
+    [switch]$Win64 = $false,
+    [switch]$NoTeletext = $false,
+    [switch]$NoPause = $false
+)
+
+Set-StrictMode -Version 3
 if (((Get-ExecutionPolicy) -ne "Unrestricted") -and ((Get-ExecutionPolicy) -ne "RemoteSigned")) {
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force -ErrorAction:SilentlyContinue
 }
@@ -6,4 +18,8 @@ Import-Module -Force -Name (Join-Path $PSScriptRoot Build-Common.psm1)
 
 $MSBuild = (Find-MSBuild)
 
-$MSBuild
+echo "MSBuild=$MSBuild"
+echo "Debug=$Debug"
+echo "Release=$Release"
+echo "Win32=$Win32"
+echo "Win64=$Win64"
