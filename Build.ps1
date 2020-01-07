c2e5676ff60280bfc06e7c9a22e8e37ce34eb5a1
Thierry Lelegard <thierry@lelegard.fr>
2020-01-07 22:46:38 +0100

    more

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,10 @@
-﻿if (((Get-ExecutionPolicy) -ne "Unrestricted") -and ((Get-ExecutionPolicy) -ne "RemoteSigned")) {
+﻿param([string]$Arch = "x64")
+
+if (((Get-ExecutionPolicy) -ne "Unrestricted") -and ((Get-ExecutionPolicy) -ne "RemoteSigned")) {
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force -ErrorAction:SilentlyContinue
 }
 Import-Module -Force -Name (Join-Path $PSScriptRoot Build-Common.psm1)
 
 $MSBuild = (Find-MSBuild)
 
-& $MSBuild "$PSScriptRoot\test.sln" /nologo /maxcpucount /property:Configuration=Release /property:Platform=x64 "/target:dll;app;app_dll"
+& $MSBuild "$PSScriptRoot\test.sln" /nologo /maxcpucount /property:Configuration=Release /property:Platform=$Arch "/target:dll;app;app_dll"
